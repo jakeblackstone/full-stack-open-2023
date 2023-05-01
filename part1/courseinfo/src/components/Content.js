dfc0921aa6b73eeff816da984626760a6d80f204
jakeblackstone <jake7405@gmail.com>
2023-05-01 17:02:51 -0600

    1.3-1.5 done

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -3,9 +3,9 @@ import Part from "./Part";
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts[0]} exercise={props.exercises[0]}/>
-      <Part part={props.parts[1]} exercise={props.exercises[1]}/>
-      <Part part={props.parts[2]} exercise={props.exercises[2]}/>
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
     </div>
   );
 };
